742e8d445f713f7c157226e160c80edc1d04da00
FujiwaraKanta <FujiwaraKanta@192.168.3.127>
2026-09-25 10:36:20 +0900

    add:Case06 FujiwaraKanta カテゴリ検索正常系テストコード
    Case06.java 46行目から137行目追加

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f02_faq/Case06.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f02_faq/Case06.java
@@ -1,6 +1,11 @@
 package jp.co.sss.lms.ct.f02_faq;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +14,9 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
 
 /**
  * 結合テスト よくある質問機能
@@ -35,42 +43,100 @@ public class Case06 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		goTo("http://localhost:8080/lms");
+		WebElement login = webDriver.findElement(By.tagName("h2"));
+		assertEquals("ログイン", login.getText());
+		getEvidence(new Object() {
+
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		WebElement loginIdInput = webDriver.findElement(By.id("loginId"));
+		loginIdInput.clear();
+		loginIdInput.sendKeys("StudentAA05");
+
+		WebElement loginPassInput = webDriver.findElement(By.id("password"));
+		loginPassInput.clear();
+		loginPassInput.sendKeys("StudentAA05");
+
+		WebElement loginButton = webDriver.findElement(By.className("btn-primary"));
+		loginButton.click();
+
+		WebElement courseDetail = webDriver.findElement(By.className("active"));
+		assertEquals("コース詳細", courseDetail.getText());
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 上部メニューの「ヘルプ」リンクからヘルプ画面に遷移")
 	void test03() {
-		// TODO ここに追加
+		WebElement menu = webDriver.findElement(By.linkText("機能"));
+		menu.click();
+
+		WebElement helpLink = webDriver.findElement(By.linkText("ヘルプ"));
+		helpLink.click();
+
+		WebElement helpCheck = webDriver.findElement(By.tagName("h2"));
+		assertEquals("ヘルプ", helpCheck.getText());
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 「よくある質問」リンクからよくある質問画面を別タブに開く")
 	void test04() {
-		// TODO ここに追加
+		WebElement faqLink = webDriver.findElement(By.linkText("よくある質問"));
+		faqLink.click();
+
+		for (String window : webDriver.getWindowHandles()) {
+			webDriver.switchTo().window(window);
+		}
+
+		WebElement faqLinkCheck = webDriver.findElement(By.tagName("h2"));
+		assertEquals("よくある質問", faqLinkCheck.getText());
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(5)
 	@DisplayName("テスト05 カテゴリ検索で該当カテゴリの検索結果だけ表示")
 	void test05() {
-		// TODO ここに追加
+		WebElement categoryLink = webDriver.findElement(By.linkText("【研修関係】"));
+		String[] checkStrings = { "キャンセル料・途中退校について", "研修の申し込みはどのようにすれば良いですか？" };
+		categoryLink.click();
+		getEvidence(new Object() {
+		});
+
+		List<WebElement> searchResults = webDriver.findElements(By.cssSelector("tr dt.mb10 span:not(.mr10)"));
+
+		for (int i = 0; i < searchResults.size(); i++) {
+			assertThat(searchResults.get(i).getText(), is(containsString(checkStrings[i])));
+		}
 	}
 
 	@Test
 	@Order(6)
 	@DisplayName("テスト06 検索結果の質問をクリックしその回答を表示")
 	void test06() {
-		// TODO ここに追加
-	}
+		WebElement question = webDriver.findElement(By.cssSelector("dt.mb10"));
+		((JavascriptExecutor) webDriver).executeScript(
+				"arguments[0].scrollIntoView({block: 'center'});", question);
+		question.click();
 
+		WebElement answer = webDriver.findElement(By.cssSelector("dd[id^='answer-h']"));
+
+		getEvidence(new Object() {
+		});
+
+		assertTrue(answer.isDisplayed());
+
+	}
 }
